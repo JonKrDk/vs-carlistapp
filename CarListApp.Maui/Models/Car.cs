7b11d0251a88b5f402da24b5d51aa5e2f38a102b
Jon Kristensen <jon@jonkr.dk>
2022-07-14 06:14:09 +0200

    Added datamodels

--- CarListApp.Maui/Models/Car.cs
+++ CarListApp.Maui/Models/Car.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace CarListApp.Maui.Models
 {
-    public class Car
+    public class Car : BaseEntity
     {
-        public int    Id    { get; set; }
         public string Make  { get; set; }
         public string Model { get; set; }
         public string Vin   { get; set; }
