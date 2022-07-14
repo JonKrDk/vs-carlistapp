df62e2ab794891936b097a03f06ba0be4cd7a203
Jon Kristensen <jon@jonkr.dk>
2022-07-14 10:59:56 +0200

    Added SQLite database

--- CarListApp.Maui/Models/Car.cs
+++ CarListApp.Maui/Models/Car.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace CarListApp.Maui.Models
 {
+    [Table("cars")]
     public class Car : BaseEntity
     {
         public string Make  { get; set; }
         public string Model { get; set; }
+
+        [MaxLength(12)]
+        [Unique]
         public string Vin   { get; set; }
     }
 }
--- CarListApp.Maui/Services/CarService.cs
+++ CarListApp.Maui/Services/CarService.cs
@@ -1,4 +1,5 @@
 ﻿using CarListApp.Maui.Models;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +10,49 @@ namespace CarListApp.Maui.Services
 {
     public class CarService
     {
+        private SQLiteConnection connection = null;
+        private string _dbPath;
+        private string StatusMessage;
+
+        public CarService(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        private void Init()
+        {
+            if (connection is null)
+            {
+                connection = new SQLiteConnection(_dbPath);
+                connection.CreateTable<Car>();
+            }
+        }
+
         public List<Car> GetCars()
         {
-            return new List<Car>()
+
+            try
             {
-                new Car { Id = 1, Make = "Honda",   Model = "Fit",    Vin = "123" },
-                new Car { Id = 2, Make = "Toyota",  Model = "Prado",  Vin = "123" },
-                new Car { Id = 3, Make = "Honda",   Model = "Civic",  Vin = "123" },
-                new Car { Id = 4, Make = "Audi",    Model = "A5",     Vin = "123" },
-                new Car { Id = 5, Make = "BMW",     Model = "M3",     Vin = "123" },
-                new Car { Id = 6, Make = "Nissan",  Model = "Note",   Vin = "123" },
-                new Car { Id = 7, Make = "Ferrari", Model = "Spider", Vin = "123" }
-            };
+                Init();
+                return connection.Table<Car>().ToList();
+            }
+            catch (Exception)
+            {
+                StatusMessage = "Failed to retrive data.";
+            }
+
+            return new List<Car>();
+
+            //return new List<Car>()
+            //{
+            //    new Car { Id = 1, Make = "Honda",   Model = "Fit",    Vin = "123" },
+            //    new Car { Id = 2, Make = "Toyota",  Model = "Prado",  Vin = "123" },
+            //    new Car { Id = 3, Make = "Honda",   Model = "Civic",  Vin = "123" },
+            //    new Car { Id = 4, Make = "Audi",    Model = "A5",     Vin = "123" },
+            //    new Car { Id = 5, Make = "BMW",     Model = "M3",     Vin = "123" },
+            //    new Car { Id = 6, Make = "Nissan",  Model = "Note",   Vin = "123" },
+            //    new Car { Id = 7, Make = "Ferrari", Model = "Spider", Vin = "123" }
+            //};
         }
     }
 }
