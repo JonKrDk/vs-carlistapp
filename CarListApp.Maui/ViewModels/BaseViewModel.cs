c48547402ad9242d04a4e4d653531f64cf291f64
Jon Kristensen <jon@jonkr.dk>
2022-07-14 07:05:58 +0200

    Added ObservableObject to auto generate change notifications

---
+++ CarListApp.Maui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Maui.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _title;
+
+        [ObservableProperty]
+        private bool   _isBusy;
+    }
+}
